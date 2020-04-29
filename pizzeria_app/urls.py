87426add78b0899c983a6af394a70443a9752672
Josh Jacobsen <joshj@entouch.net>
2020-04-29 01:23:20 -0500

    Create individual pizza pages to view toppings

--- pizzeria_app/urls.py
+++ pizzeria_app/urls.py
@@ -6,4 +6,5 @@ app_name = 'pizzeria_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('pizzas', views.pizzas, name='pizzas'),
+    path('pizzas/<int:pizza_id>/', views.pizza, name='pizza'),
 ]
--- pizzeria_app/views.py
+++ pizzeria_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Pizza
+from .models import Pizza, Topping
 
 # Create your views here.
 
@@ -11,3 +11,9 @@ def pizzas(request):
     pizzas = Pizza.objects.order_by('pizza_name')
     context = {'pizzas':pizzas}
     return render(request, 'pizzeria_app/pizzas.html', context)
+
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+    pizza_with_toppings = pizza.topping_set.order_by('topping_name')
+    context = {'pizza':pizza, 'pizza_with_toppings':pizza_with_toppings}
+    return render(request, 'pizzeria_app/pizza.html', context)
