b06533965b719130b91f14583d7239ce10cd7eb5
Josh Jacobsen <joshj@entouch.net>
2020-04-29 00:31:10 -0500

    Setup views and urls for base.html and index.html

---
+++ pizzeria_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'pizzeria_app'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    
+]
--- pizzeria_app/views.py
+++ pizzeria_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """The home page for our Pizzeria"""
+    return render(request, 'pizzeria_app/index.html')
