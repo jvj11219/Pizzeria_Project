3b3c2001d2ce667d10b4288b9fd69d9f755f0f7c
Josh Jacobsen <joshj@entouch.net>
2020-04-29 03:14:35 -0500

    Rename Comment and Topping foreign key field

--- pizzeria_app/models.py
+++ pizzeria_app/models.py
@@ -9,14 +9,14 @@ class Pizza(models.Model):
         return self.pizza_name
 
 class Topping(models.Model):
-    pizza_name = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    pizza_fk = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     topping_name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.topping_name
 
 class Comment(models.Model):
-    pizza_name = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    pizza_fk = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     comment_text = models.TextField()
     comment_author = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
