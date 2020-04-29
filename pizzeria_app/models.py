0393a55acd63a21dcd8a860def862cc56d570c60
Josh Jacobsen <joshj@entouch.net>
2020-04-29 00:17:12 -0500

    Create 'Comment' model, migrate, and register it

--- pizzeria_app/models.py
+++ pizzeria_app/models.py
@@ -14,3 +14,11 @@ class Topping(models.Model):
 
     def __str__(self):
         return self.topping_name
+
+class Comment(models.Model):
+    pizza_name = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment_text
